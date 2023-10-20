ed8d75e0f67362efb14f4c7da2012fb1574a332b
mathnebula186f <getgopalbansal@gmail.com>
2023-10-21 00:44:59 +0530

    Changed to vite

---
+++ dating-app/client/src/SocketProvider.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useMemo, useContext } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  const socket = useContext(SocketContext);
+  return socket;
+};
+
+export const SocketProvider = (props) => {
+  const socket = useMemo(
+    () => io("https://messaging-app-j1xy.vercel.app/"),
+    []
+  );
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {props.children}
+    </SocketContext.Provider>
+  );
+};
